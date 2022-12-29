3940c3e4c21a262fb6bbe86a28b52bac8ef03387
nguyennv <nvnguyen2504@gmail.com>
2022-12-29 11:57:46 +0700

    MainApi.cs

---
+++ AD/AD/MainApi.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.UI;
+
+namespace AD
+{
+    public class MainApi : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            var ui = new SetupInterface();
+            ui.Initialize(application);
+            return Result.Succeeded;
+        }
+    }
+}
